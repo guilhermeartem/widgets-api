7d7094008352e2c2752fc5c175d37f1face92d11
Guilherme Artém <guilherme.artem@gmail.com>
2017-10-22 20:01:57 -0200

    servindo a documentação do swagger no /docs

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ import (
 
 var validate *validator.Validate
 
+func redirectDocs(w http.ResponseWriter, r *http.Request) {
+
+	http.Redirect(w, r, "/docs/", 301)
+}
+
 func main() {
 	validate = validator.New()
 	defer db.Close()
 
 	router := NewRouter()
+	router.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs/"))))
+	router.HandleFunc("/docs", redirectDocs)
 
 	log.Fatal(http.ListenAndServe(":4000", router))
 }
